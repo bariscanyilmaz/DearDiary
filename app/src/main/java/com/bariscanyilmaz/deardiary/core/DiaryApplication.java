0cc43a4fa5b06c2abc0c891c4efdcb039ddc309c
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-06-08 01:44:08 +0300

    add hilt di to project

---
+++ app/src/main/java/com/bariscanyilmaz/deardiary/core/DiaryApplication.java
@@ -0,0 +1,10 @@
+package com.bariscanyilmaz.deardiary.core;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class DiaryApplication extends Application {
+
+}
